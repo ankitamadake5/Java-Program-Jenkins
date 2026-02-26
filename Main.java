36242e3837398addee2efbf9a3591cadea17f6ce
ankitamadake5 <105648490+ankitamadake5@users.noreply.github.com>
2026-02-26 14:48:22 +0530

    initial commit

--- Main.java
+++ Main.java
@@ -6,5 +6,6 @@ public class Main
         System.out.println("This is java Program");
         System.out.println("Output from java Program");
         System.out.println(new Date());
+         System.out.println("Hello");
    }
 }
