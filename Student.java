02acef8b1183149a5fa1610eb3b0330bfb87d5fb
ankitamadake5 <105648490+ankitamadake5@users.noreply.github.com>
2026-02-26 15:37:28 +0530

    added student class

---
+++ Student.java
@@ -0,0 +1,14 @@
+import java.util.Date;
+
+public class Student
+{
+public Student()
+{
+  System.out.println("Creating Student Object");
+}
+public void sayHello()
+{
+    System.out.println("Hi I am Student);
+}
+   
+}
